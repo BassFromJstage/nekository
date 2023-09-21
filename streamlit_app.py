383b9ab97e1d947070fa51a8d15fbb7bf0cbeec5
BassFromJstage <144981117+BassFromJstage@users.noreply.github.com>
2023-09-21 04:37:11 +0000

    ｙ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import random
 
 st.title('おみくじアプリ')
 
-if st.button('おみくじを引く'):
+if st.button('2023最新の幸運を入手'):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
     st.write(f"結果:{result}")
